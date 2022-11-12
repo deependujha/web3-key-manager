063014f68d705876d709de428ed6aa7f92de9669
deependujha <deependujha21@gmail.com>
2022-11-12 20:06:26 +0530

    card screen of the landing page modified

--- frontend/components/landingPage/Home_screen.jsx
+++ frontend/components/landingPage/Home_screen.jsx
@@ -1,27 +1,29 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
 const Home_screen = () => {
-  return (
-    <div className=" bg-gradient-to-r from-[#d13049] to-[#f2cbcb]">
-      <div className="grid grid-cols-2 gap-4">
-        <div className="flex justify-center sm:justify-end my-auto">
-          <div>
-            <p className="text-xl sm:text-4xl md:text-6xl lg:text-8xl">Hello</p>
-            <p className="text-xl sm:text-4xl md:text-6xl lg:text-8xl">Deependu</p>
-          </div>
-        </div>
-        <div className=" flex justify-center">
-          <Image
-            src={`/images/homeScreen/screen_img.svg`}
-            width="639"
-            height="200"
-            alt="loading..."
-          />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className=" bg-gradient-to-r from-[#d13049] to-[#f2cbcb]">
+			<div className="grid grid-cols-2 gap-4">
+				<div className="mx-20 justify-center my-auto">
+					<div>
+						<div className="text-xl sm:text-4xl md:text-6xl lg:text-8xl">
+							WEB3-key-manager
+							<div>- Your decentralized password manager</div>
+						</div>
+					</div>
+				</div>
+				<div className=" flex justify-center">
+					<Image
+						src={`/images/homeScreen/screen_img.svg`}
+						width="639"
+						height="200"
+						alt="loading..."
+					/>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Home_screen;
--- frontend/components/landingPage/Info.jsx
+++ frontend/components/landingPage/Info.jsx
@@ -1,40 +1,40 @@
-import React from "react";
-import Card from "./CardScreen";
+import React from 'react';
+import Card from './CardScreen';
 
 const Info = () => {
-  return (
-    <div className="container mx-auto my-10">
-      <div className="text-4xl flex justify-center">
-        <p> The autopilot for all your passwords </p>
-      </div>
-      <div className="grid grid-cols-3 gap-6 my-10">
-        <div>
-          <Card />
-        </div>
-        <div>
-          {" "}
-          <Card />
-        </div>
-        <div>
-          {" "}
-          <Card />
-        </div>
-      </div>
-      <div className="grid grid-cols-3 gap-6 my-10">
-        <div>
-          <Card />
-        </div>
-        <div>
-          {" "}
-          <Card />
-        </div>
-        <div>
-          {" "}
-          <Card />
-        </div>
-      </div>
-    </div>
-  );
+	return (
+		<div className="container mx-auto my-10">
+			<div className="text-4xl flex justify-center my-5">
+				<p> One stop solution for all your passwords 🔒🔑 </p>
+			</div>
+			<div className="md:grid md:grid-cols-3 ">
+				<div className="mx-5 my-3">
+					<Card />
+				</div>
+				<div className="mx-5 my-3">
+					{' '}
+					<Card />
+				</div>
+				<div className="mx-5 my-3">
+					{' '}
+					<Card />
+				</div>
+			</div>
+			<div className="md:grid md:grid-cols-3 ">
+				<div className="mx-5 my-3">
+					<Card />
+				</div>
+				<div className="mx-5 my-3">
+					{' '}
+					<Card />
+				</div>
+				<div className="mx-5 my-3">
+					{' '}
+					<Card />
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Info;
