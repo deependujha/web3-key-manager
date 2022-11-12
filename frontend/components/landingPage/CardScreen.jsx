111a5870c4d762131f738411a52a1d4711991fbd
deependujha <deependujha21@gmail.com>
2022-11-12 21:02:06 +0530

    images added in home page

--- frontend/components/landingPage/CardScreen.jsx
+++ frontend/components/landingPage/CardScreen.jsx
@@ -1,24 +1,21 @@
-import React from "react";
-import { Card } from "flowbite-react";
+import React from 'react';
+import { Card } from 'flowbite-react';
+import Image from 'next/image';
 
-const CardScreen = ({image , heading , text}) => {
-  return (
-    <Card>
-      <div className="flex flex-col items-center pb-10">
-        <img
-          className="mb-3 h-24 w-24 rounded-full shadow-lg"
-          src="https://flowbite.com/docs/images/people/profile-picture-3.jpg"
-          alt="Bonnie image"
-        />
-        <div className="mb-1 text-xl text-gray-900 dark:text-white font-bold">
-          {heading}
-        </div>
-        <span className="text-sm text-gray-500 dark:text-gray-400 my-2">
-          {text}
-        </span>
-      </div>
-    </Card>
-  );
+const CardScreen = ({ img, heading, text }) => {
+	return (
+		<Card>
+			<div className="flex flex-col items-center pb-10">
+				<Image src={img} alt="Bonnie image" height={60} width={60} />
+				<div className="mb-1 text-xl text-gray-900 dark:text-white font-bold">
+					{heading}
+				</div>
+				<div className="text-sm text-gray-500 dark:text-gray-400 my-2">
+					{text}
+				</div>
+			</div>
+		</Card>
+	);
 };
 
 export default CardScreen;
