fb87c6898aa8ab82bfe61f4b1bf341560a26d9c3
Niteshkumar <nkumar35101@gmail.com>
2022-11-12 20:44:46 +0530

    content of info section is done

--- frontend/components/landingPage/Home_screen.jsx
+++ frontend/components/landingPage/Home_screen.jsx
@@ -3,27 +3,27 @@ import Image from 'next/image';
 
 const Home_screen = () => {
 	return (
-		<div className=" bg-gradient-to-r from-[#d13049] to-[#f2cbcb]">
-			<div className="grid grid-cols-2 gap-4">
-				<div className="mx-20 justify-center my-auto">
-					<div>
-						<div className="text-xl sm:text-4xl md:text-6xl lg:text-8xl">
-							WEB3-key-manager
-							<div>- Your decentralized password manager</div>
-						</div>
-					</div>
-				</div>
-				<div className=" flex justify-center">
-					<Image
-						src={`/images/homeScreen/screen_img.svg`}
-						width="639"
-						height="200"
-						alt="loading..."
-					/>
-				</div>
-			</div>
-		</div>
-	);
+    <div className=" bg-gradient-to-r from-[#d13049] to-[#f2cbcb]">
+      <div className="grid grid-cols-2 gap-4">
+        <div className="mx-20 justify-center my-auto">
+          <div>
+            <div className="text-xl sm:text-4xl md:text-6xl lg:text-8xl font-bold">
+              WEB3-Key-Manager
+              <div>- Your decentralized password manager</div>
+            </div>
+          </div>
+        </div>
+        <div className=" flex justify-center">
+          <Image
+            src={`/images/homeScreen/screen_img.svg`}
+            width="639"
+            height="200"
+            alt="loading..."
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home_screen;
