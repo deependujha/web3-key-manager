c28cde9a6d3654836662668c7bed8eb54a12655e
deependujha <deependujha21@gmail.com>
2022-11-12 18:19:39 +0530

    connection with metamask done

--- frontend/components/landingPage/Nav_baar.jsx
+++ frontend/components/landingPage/Nav_baar.jsx
@@ -1,11 +1,51 @@
 import React from 'react';
 import { Navbar, Button } from 'flowbite-react';
+import { ethers } from 'ethers';
 import Link from 'next/link';
 import Image from 'next/image';
 
 const Nav_baar = ({ currPage }) => {
+	// Function for connecting the app with wallet
 	const connectWallet = async () => {
-		console.log("Clicked on 'connect_wallet'");
+		// A Web3Provider wraps a standard Web3 provider, which is
+		// what MetaMask injects as window.ethereum into each page
+		let provider = new ethers.providers.Web3Provider(window.ethereum);
+		// to check with which network metamask is currently connected, and show required alert
+		const { chainId } = await provider.getNetwork();
+
+		// if connected chain is not 'Goerli', switch network.
+		if (chainId !== 5) {
+			ethereum.request({
+				method: 'wallet_switchEthereumChain',
+				params: [
+					{
+						chainId: '0x5',
+					},
+				],
+			});
+		}
+		
+		// MetaMask requires requesting permission to connect users accounts
+		await provider.send('eth_requestAccounts', []);
+
+		// The MetaMask plugin also allows signing transactions to
+		// send ether and pay to change state within the blockchain.
+		// For this, you need the account signer...
+		let signer = provider.getSigner();
+		console.log('connected with metamask');
+		console.log("user's address: ", await signer.getAddress());
+
+		// creating an instance of the smart contract
+		// it takes three parameters => {contract's address, abi, signer/provider}
+		// provider can perform read only transactions
+		// signer can perform all kind of transactions
+		// let contract = new ethers.Contract(
+		// 	'0x57b8556A95ecc904F58Fc5876CBEF62ab0f29F5f',
+		// 	ABI,
+		// 	signer
+		// );
+		// setMyContract(contract);
+		// console.log(contract.address);
 	};
 
 	return (
--- frontend/pages/about.js
+++ frontend/pages/about.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import { useEffect } from 'react';
+import styles from '../styles/Home.module.css';
 
 const about = ({ setCurrPage }) => {
 	useEffect(() => {
 		setCurrPage('about');
 	}, []);
 
-	return <div>about</div>;
+	return (
+		<div className={styles.container}>
+			<div>about</div>
+		</div>
+	);
 };
 
 export default about;
