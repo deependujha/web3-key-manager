c70c7e69e09d756f83d47f34f5c80de61ed58459
Niteshkumar <nkumar35101@gmail.com>
2022-11-12 22:11:37 +0530

    hover effect added through motion framer

--- frontend/components/landingPage/CardScreen.jsx
+++ frontend/components/landingPage/CardScreen.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Card } from 'flowbite-react';
 import Image from 'next/image';
+import { motion } from "framer-motion";
+
 
 const CardScreen = ({ img, heading, text }) => {
-	return (
-		<Card>
-			<div className="flex flex-col items-center pb-10">
-				<Image src={img} alt="Bonnie image" height={60} width={60} />
-				<div className="mb-1 text-xl text-gray-900 dark:text-white font-bold">
-					{heading}
-				</div>
-				<div className="text-sm text-gray-500 dark:text-gray-400 my-2">
-					{text}
-				</div>
-			</div>
-		</Card>
-	);
+  return (
+    <motion.div whileHover={{ scale: 0.9 }} whileTap={{ scale: 0.9 }}>
+      <Card>
+        <div className="flex flex-col items-center pb-10">
+          <Image src={img} alt="Bonnie image" height={60} width={60} />
+          <div className="mb-1 text-xl text-gray-900 dark:text-white font-bold">
+            {heading}
+          </div>
+          <div className="text-sm text-gray-500 dark:text-gray-400 my-2">
+            {text}
+          </div>
+        </div>
+      </Card>
+    </motion.div>
+  );
 };
 
 export default CardScreen;
--- frontend/components/landingPage/Info.jsx
+++ frontend/components/landingPage/Info.jsx
@@ -3,62 +3,62 @@ import Card from './CardScreen';
 
 const Info = () => {
 	return (
-		<div className="container mx-auto my-10">
-			<div className="text-4xl flex justify-center my-5 font-bold">
-				<p> One Stop Solution For All Your Passwords 🔒🔑 </p>
-			</div>
-			<div className="md:grid md:grid-cols-3 ">
-				<div className="mx-5 my-3">
-					<Card
-						img="/images/homeScreen/wallet.png"
-						heading="Connect With Your Wallet"
-						text="Securily login with your metamask wallet and sign-in into Web3-Key-Manager."
-					/>
-				</div>
-				<div className="mx-5 my-3">
-					{' '}
-					<Card
-						img="/images/homeScreen/shield.png"
-						heading="Store Your Password Securely"
-						text="Store all of your passwords securely in encrypted format on IPFS."
-					/>
-				</div>
-				<div className="mx-5 my-3">
-					{' '}
-					<Card
-						img="/images/homeScreen/blockchain.png"
-						heading="Leverage The Power Of Blockchain"
-						text="All your passwords are encrypted using LIT protocol, and are stored on decentralized storages (IPFS)."
-					/>
-				</div>
-			</div>
-			<div className="md:grid md:grid-cols-3 ">
-				<div className="mx-5 my-3">
-					<Card
-						img="/images/homeScreen/padlock.png"
-						heading="Generate Random Passwords"
-						text="Generate strong, hard to crack and random passwords for all your applications."
-					/>
-				</div>
-				<div className="mx-5 my-3">
-					{' '}
-					<Card
-						img="/images/homeScreen/jigsaw.png"
-						heading="Contribute To The Comunity"
-						text="If you like our initiative, you can contribute to it by visiting 'Contribute' section."
-					/>
-				</div>
-				<div className="mx-5 my-3">
-					{' '}
-					<Card
-						img="/images/homeScreen/best-product.png"
-						heading="Purchase For More "
-						text="Buy our premium plans to store more password secuely."
-					/>
-				</div>
-			</div>
-		</div>
-	);
+    <div className="container mx-auto my-10">
+      <div className="text-4xl flex justify-center my-5 font-bold">
+        <p> One Stop Solution For All Your Passwords 🔒🔑 </p>
+      </div>
+      <div className="md:grid md:grid-cols-3 ">
+        <div className="mx-5 my-3">
+          <Card
+            img="/images/homeScreen/wallet.png"
+            heading="Connect With Your Wallet"
+            text="Securily login with your metamask wallet and sign-in into Web3-Key-Manager."
+          />
+        </div>
+        <div className="mx-5 my-3">
+          {" "}
+          <Card
+            img="/images/homeScreen/shield.png"
+            heading="Store Your Password Securely"
+            text="Store all of your passwords securely in encrypted format on Interplanetary File System (IPFS)."
+          />
+        </div>
+        <div className="mx-5 my-3">
+          {" "}
+          <Card
+            img="/images/homeScreen/blockchain.png"
+            heading="Leverage The Power Of Blockchain"
+            text="All your passwords are encrypted using LIT protocol, and are stored on decentralized storages (IPFS)."
+          />
+        </div>
+      </div>
+      <div className="md:grid md:grid-cols-3 ">
+        <div className="mx-5 my-3">
+          <Card
+            img="/images/homeScreen/padlock.png"
+            heading="Generate Random Passwords"
+            text="Generate strong, hard to crack and random passwords for all your applications."
+          />
+        </div>
+        <div className="mx-5 my-3">
+          {" "}
+          <Card
+            img="/images/homeScreen/jigsaw.png"
+            heading="Contribute To The Comunity"
+            text="If you like our initiative, you can contribute to it by visiting 'Contribute' section."
+          />
+        </div>
+        <div className="mx-5 my-3">
+          {" "}
+          <Card
+            img="/images/homeScreen/best-product.png"
+            heading="Purchase For More "
+            text="Buy our premium plans to store more password secuely on the blockchain."
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Info;
