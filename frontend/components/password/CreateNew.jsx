de93d5d2d163344bc0f6bea88bc8c357c04ae550
deependujha <deependujha21@gmail.com>
2022-11-15 23:43:08 +0530

    'sweet-alert' added in the project

--- frontend/components/password/CreateNew.jsx
+++ frontend/components/password/CreateNew.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import CreateRandomPassword from './CreateRandomPassword';
 import TypePassword from './TypePassword';
 import { Card, Button } from 'flowbite-react';
+import Swal from 'sweetalert2';
 
 const CreateNew = () => {
 	let newPasswd = {
@@ -18,71 +19,107 @@ const CreateNew = () => {
 		setCreateNew({ ...createNew, password: val });
 	};
 
+	const saveOnBlockChain = () => {
+		if (createNew.project === '') {
+			// alert('Please write the application/project name');
+			Swal.fire({
+				icon: 'error',
+				title: 'Oops...',
+				text: 'Please write the application/project name!',
+				confirmButtonColor: '#D6465B',
+			});
+			return;
+		}
+		if (createNew.password === '') {
+			if (createNew.toogle === false) {
+				// alert('Please generate the random password first');
+				Swal.fire({
+					icon: 'error',
+					title: 'Oops...',
+					text: 'Please generate the random password first!',
+					confirmButtonColor: '#D6465B',
+				});
+			} else {
+				// alert('Please write your password');
+				Swal.fire({
+					icon: 'error',
+					title: 'Oops...',
+					text: 'Please write your password!',
+					confirmButtonColor: '#D6465B',
+				});
+			}
+			return;
+		}
+
+		console.log('data is: ', createNew);
+	};
+
 	return (
-    <div
-      style={{ width: "70vw", backgroundColor: "white", maxWidth: "70vw" }}
-      className="my-6"
-    >
-      <div className="px-8">
-        <label
-          htmlFor="first_name"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          Application/ Project name
-        </label>
-        <input
-          onChange={(e) => {
-            setCreateNew({ ...createNew, project: e.target.value });
-          }}
-          name="project"
-          type="text"
-          className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          placeholder="facebook"
-          required
-        />
-      </div>
-      <div className='my-6 px-8'>
-        <label
-          htmlFor="default-toggle"
-          className="inline-flex relative items-center cursor-pointer"
-        >
-          <input
-            name="toogle"
-            type="checkbox"
-            value={createNew.toogle}
-            onChange={() => {
-              setCreateNew({ ...createNew, toogle: !createNew.toogle });
-            }}
-            id="default-toggle"
-            className="sr-only peer"
-          />
-          <div className=" w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-          <span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
-            Create custom password
-          </span>
-        </label>
-      </div>
-      {createNew.toogle ? (
-        <TypePassword
-          password={createNew.password}
-          setPassword={passwdChange}
-        />
-      ) : (
-        <CreateRandomPassword
-          password={createNew.password}
-          directlySetPassword={directlySetPassword}
-        />
-      )}
-      <div className='px-8 my-6'>
-        <Button
-          onClick={() => console.log("data is: ", createNew)}
-          gradientDuoTone="pinkToOrange"
-        >
-          save securely on BlockChain
-        </Button>
-      </div>
-    </div>
-  );
+		<div
+			style={{ width: '70vw', backgroundColor: 'white', maxWidth: '70vw' }}
+			className="my-6"
+		>
+			<div className="px-8">
+				<label
+					htmlFor="first_name"
+					className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+				>
+					Application/ Project name
+				</label>
+				<input
+					onChange={(e) => {
+						setCreateNew({ ...createNew, project: e.target.value });
+					}}
+					name="project"
+					type="text"
+					className=" bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+					placeholder="facebook"
+					required
+				/>
+			</div>
+			<div className="my-6 px-8">
+				<label
+					htmlFor="default-toggle"
+					className="inline-flex relative items-center cursor-pointer"
+				>
+					<input
+						name="toogle"
+						type="checkbox"
+						value={createNew.toogle}
+						onChange={() => {
+							setCreateNew({
+								...createNew,
+								toogle: !createNew.toogle,
+								password: '',
+							});
+						}}
+						id="default-toggle"
+						className="sr-only peer"
+					/>
+					<div className=" w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+					<span className="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">
+						Create custom password
+					</span>
+				</label>
+			</div>
+			{createNew.toogle ? (
+				<TypePassword
+					password={createNew.password}
+					setPassword={passwdChange}
+				/>
+			) : (
+				<CreateRandomPassword
+					password={createNew.password}
+					directlySetPassword={directlySetPassword}
+				/>
+			)}
+			<div className="px-8 my-6">
+				<Button onClick={saveOnBlockChain} gradientDuoTone="pinkToOrange">
+					save securely on BlockChain
+				</Button>
+			</div>
+		</div>
+	);
 };
 
 export default CreateNew;
--- frontend/components/password/CreateRandomPassword.jsx
+++ frontend/components/password/CreateRandomPassword.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Card, Button } from 'flowbite-react';
 
-const CreateRandomPassword = ({password, directlySetPassword}) => {
+const CreateRandomPassword = ({ password, directlySetPassword }) => {
 	const [length, setLength] = useState(14);
 	const [copytxt, setCopytxt] = useState('copy');
 
@@ -29,48 +29,44 @@ const CreateRandomPassword = ({password, directlySetPassword}) => {
 	};
 
 	return (
-    <div style={{ width: "70vw", backgroundColor: "white", maxWidth: "70vw" }}>
-      <div className="px-8 my-6">
-        <label
-          htmlFor="minmax-range"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          password length: {length}
-        </label>
-        <input
-          id="minmax-range"
-          type="range"
-          min="4"
-          max="16"
-          name="length"
-          value={length}
-          onChange={(e) => {
-            setLength(e.target.value);
-          }}
-          className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
-        />
-      </div>
-      <div className='my-6 px-8'>
-        <Button
-          onClick={generateP}
-          gradientDuoTone="pinkToOrange"
-        >
-          Generate password
-        </Button>
+		<div style={{ width: '70vw', backgroundColor: 'white', maxWidth: '70vw' }}>
+			<div className="px-8 my-6">
+				<label
+					htmlFor="minmax-range"
+					className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+				>
+					password length: {length}
+				</label>
+				<input
+					id="minmax-range"
+					type="range"
+					min="4"
+					max="16"
+					name="length"
+					value={length}
+					onChange={(e) => {
+						setLength(e.target.value);
+					}}
+					className="w-full h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer dark:bg-gray-700"
+				/>
+			</div>
+			<div className="my-6 px-8">
+				<Button onClick={generateP} gradientDuoTone="pinkToOrange">
+					Generate password
+				</Button>
 
-		  
-        <div className='my-1'>{password === "" ? "Generate password" : "your password is:"}</div>
-        {password !== "" && (
-          <div className='flex justify-between'>
-            <div className="my-2"> {password}</div>
-            <Button gradientMonochrome="failure" onClick={copyPassword}>
-              {copytxt}
-            </Button>
-          </div>
-        )}
-      </div>
-    </div>
-  );
+				<div className="my-1">{password === '' ? '' : 'your password is:'}</div>
+				{password !== '' && (
+					<div className="flex justify-between">
+						<div className="my-2"> {password}</div>
+						<Button gradientMonochrome="failure" onClick={copyPassword}>
+							{copytxt}
+						</Button>
+					</div>
+				)}
+			</div>
+		</div>
+	);
 };
 
 export default CreateRandomPassword;
