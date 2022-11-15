09923c1ca0dce62c67f089e0ceeb951fde945e7d
deependujha <deependujha21@gmail.com>
2022-11-15 15:49:42 +0530

    password component in working in small devices as well

---
+++ frontend/components/password/PasswordComponent.jsx
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import CreateRandomPassword from './CreateRandomPassword';
+import YourPasswords from './YourPasswords';
+
+const PasswordComponent = () => {
+	const [current, setCurrent] = useState('saved');
+
+	return (
+		<div className="w-full bg-white rounded-lg border shadow-md dark:bg-gray-800 dark:border-gray-700">
+			<div className="sm:hidden">
+				<label htmlFor="tabs" className="sr-only">
+					Select tab
+				</label>
+				<select
+					onChange={(e) => setCurrent(e.target.value)}
+					id="tabs"
+					className="bg-gray-50 border-0 border-b border-gray-200 text-gray-900 sm:text-sm rounded-t-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+				>
+					<option value="saved">Your passwords</option>
+					<option value="create">Create new password</option>
+				</select>
+			</div>
+			<ul
+				className="hidden text-sm font-medium text-center text-gray-500 rounded-lg divide-x divide-gray-200 sm:flex dark:divide-gray-600 dark:text-gray-400"
+				id="fullWidthTab"
+				data-tabs-toggle="#fullWidthTabContent"
+				role="tablist"
+			>
+				<li className="w-full">
+					<button
+						onClick={() => setCurrent('saved')}
+						className="inline-block p-4 w-full bg-gray-50 rounded-tl-lg hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600 "
+					>
+						Your passwords
+					</button>
+				</li>
+				<li className="w-full">
+					<button
+						onClick={() => setCurrent('create')}
+						className="inline-block p-4 w-full bg-gray-50 rounded-tl-lg hover:bg-gray-100 focus:outline-none dark:bg-gray-700 dark:hover:bg-gray-600"
+					>
+						Create new password
+					</button>
+				</li>
+			</ul>
+			<div
+				id="fullWidthTabContent"
+				className="border-t border-gray-200 dark:border-gray-600"
+			>
+				{current === 'create' ? (
+					<div>
+						<CreateRandomPassword />
+					</div>
+				) : (
+					<div>
+						<YourPasswords />
+					</div>
+				)}
+			</div>
+		</div>
+	);
+};
+
+export default PasswordComponent;
--- frontend/pages/password.js
+++ frontend/pages/password.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import CreateRandomPassword from '../components/password/CreateRandomPassword';
+import PasswordComponent from '../components/password/PasswordComponent';
 import YourPasswords from '../components/password/YourPasswords';
 import styles from '../styles/Home.module.css';
 
@@ -10,10 +11,13 @@ const password = ({ setCurrPage }) => {
 	}, []);
 
 	return (
-		<div className={`${styles.container} flex items-center justify-center h-screen bg-gradient-to-r from-[#d13049] to-[#f2cbcb]`}>
+		<div
+			className={`${styles.container} flex items-center justify-center h-screen bg-gradient-to-r from-[#d13049] to-[#f2cbcb]`}
+		>
 			<div>
 				{/* <CreateRandomPassword /> */}
-				<YourPasswords />
+				{/* <YourPasswords /> */}
+				<PasswordComponent />
 			</div>
 		</div>
 	);
