ee55dfae55eb8c204e65aa88be5ff3ff77abb152
Niteshkumar <nkumar35101@gmail.com>
2022-11-16 11:36:47 +0530

    connection with metamask is being checked

--- frontend/pages/password.js
+++ frontend/pages/password.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import { useEffect } from 'react';
-import CreateRandomPassword from '../components/password/CreateRandomPassword';
-import PasswordComponent from '../components/password/PasswordComponent';
-import YourPasswords from '../components/password/YourPasswords';
-import styles from '../styles/Home.module.css';
+import React from "react";
+import { useEffect } from "react";
+import CreateRandomPassword from "../components/password/CreateRandomPassword";
+import PasswordComponent from "../components/password/PasswordComponent";
+import YourPasswords from "../components/password/YourPasswords";
+import styles from "../styles/Home.module.css";
+import { useSelector } from "react-redux";
+import MetaMaskConn from "../components/extras/MetaMaskConn";
 
 const password = ({ setCurrPage }) => {
-	useEffect(() => {
-		setCurrPage('password');
-	}, []);
+  const getSigner = useSelector((state) => state.usr.signer);
 
-	return (
-		<div
-			className={`${styles.container} flex items-center justify-center h-screen bg-gradient-to-r from-[#d13049] to-[#f2cbcb]`}
-		>
-			<div>
-				<PasswordComponent />
-			</div>
-		</div>
-	);
+  useEffect(() => {
+    setCurrPage("password");
+  }, []);
+
+  return (
+    <div
+      className={`${styles.container} flex items-center justify-center h-screen bg-gradient-to-r from-[#d13049] to-[#f2cbcb]`}
+    >
+      {getSigner === "" ? (
+        <div>
+          {" "}
+          <MetaMaskConn />{" "}
+        </div>
+      ) : (
+        <div>
+          <PasswordComponent />
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default password;
