990e9ed12655148a64fd7ac1bbfeeb09f4ece447
Niteshkumar <nkumar35101@gmail.com>
2022-11-16 12:15:02 +0530

    loading page completed

--- frontend/components/password/YourPasswords.jsx
+++ frontend/components/password/YourPasswords.jsx
@@ -3,11 +3,14 @@ import KeyValue from './KeyValue';
 import { useSelector } from 'react-redux';
 import axios from 'axios';
 import lit from '../../lit_protocol/lit';
+import Loading from '../extras/Loading';
 
 const YourPasswords = () => {
 	const myContract = useSelector((state) => state.usr.contract);
 	const [myKeys, setMyKeys] = useState([]);
 	const [loading, setLoading] = useState(false);
+
+	
 
 	// const passwd = [
 	// 	{ key: 'facebook', value: 'deependu' },
@@ -83,13 +86,23 @@ const YourPasswords = () => {
 	useEffect(() => {
 		myFunc();
 	}, []);
+	
 	return (
-		<div style={{ width: '70vw', backgroundColor: 'white' }}>
-			{myKeys.map((pass, index) => (
-				<KeyValue key={index} name={pass.key} value={pass.value} />
-			))}
-		</div>
-	);
+    <div>
+      {loading === true ? (
+        <div>
+          {" "}
+          <Loading />{" "}
+        </div>
+      ) : (
+        <div style={{ width: "70vw", backgroundColor: "white" }}>
+          {myKeys.map((pass, index) => (
+            <KeyValue key={index} name={pass.key} value={pass.value} />
+          ))}
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default YourPasswords;
